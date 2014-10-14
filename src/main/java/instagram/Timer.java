66ce8eef76ae1f9de86fe9036cd770c2a5a04797
Håkon Botnmark Jahre <haakobja@gmail.com>
2014-10-14 15:29:07 +0200

    Added timed pull from Instagram

--- src/main/java/instagram/Timer.java
+++ src/main/java/instagram/Timer.java
@@ -1,8 +1,13 @@
 package instagram;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import model.instagram.InstagramData;
+import model.instagram.InstagramJSON;
+
 import javax.ejb.Schedule;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
+import java.net.URL;
 
 @Startup
 @Singleton
@@ -11,6 +16,19 @@ public class Timer {
     @Schedule(hour = "*", minute = "*")
     void updateFeed()
     {
-        System.out.println("Time!");
+        try
+        {
+            URL url = new URL("https://api.instagram.com/v1/tags/javaBin/media/recent?client_id=f05c3a9ab28f465bb9cb2c5d7c002dce");
+            ObjectMapper mapper = new ObjectMapper();
+            InstagramJSON root = mapper.readValue(url, InstagramJSON.class);
+
+            for(InstagramData d: root.getData()) {
+                System.out.println(d.toString());
+            }
+        } catch (Exception e)
+        {
+            System.err.println(e.toString());
+        }
+
     }
 }
